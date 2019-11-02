057c92738ef01511679bbb31f3c630d7977b3226
Nickolai Whitlow <ananasviolet@fintmcwells.com>
2019-11-02 15:29:06 -0700

    Grace

--- Grace.c
+++ Grace.c
@@ -3,32 +3,8 @@
 */
 #include <fcntl.h> // open
 #include <unistd.h> // write
+#include <sys/stat.h> // chmod
 #define PRINT(FD, STR) for (int i = 0; STR[i]; i++) write(FD, &(STR[i]), 1);
 #define PUTS(FD, STR) PRINT(FD, STR) write(FD, (char[1]){10}, 1);
-#define MAIN(S) int main() { \
-	int fd = open("Grace_kid.c", 514); \
-	PUTS(fd, S[0]); \
-	PUTS(fd, S[1]); \
-	PUTS(fd, S[2]); \
-	PUTS(fd, S[3]); \
-	PUTS(fd, S[4]); \
-	PUTS(fd, S[5]); \
-	PUTS(fd, S[6]); \
-	PUTS(fd, S[7]); \
-	PRINT(fd, S[8]); \
-	PRINT(fd, #S); \
-	PUTS(fd, S[9]); \
-	close(fd); \
-}
-MAIN(((char *[10]){ \
-		"/*", \
-		"	Grace", \
-		"*/", \
-		"#include <fcntl.h> // open", \
-		"#include <unistd.h> // write", \
-		"#define PRINT(FD, STR) for (int i = 0; STR[i]; i++) write(FD, &(STR[i]), 1);", \
-		"#define PUTS(FD, STR) PRINT(FD, STR) write(FD, (char[1]){10}, 1);", \
-		"#define MAIN(S) int main() { int fd = open(Grace_kid.c, 514); PUTS(fd, S[0]); PUTS(fd, S[1]); PUTS(fd, S[2]); PUTS(fd, S[3]); PUTS(fd, S[4]); PUTS(fd, S[5]); PUTS(fd, S[6]); PUTS(fd, S[7]); PRINT(fd, S[8]); PRINT(fd, #S); PUTS(fd, S[9]); close(fd); }", \
-		"MAIN(", \
-		")"
-}))
+#define MAIN(S) int main() { int fd = open(S[0], 514); PUTS(fd, S[1]); PUTS(fd, S[2]); PUTS(fd, S[3]); PUTS(fd, S[4]); PUTS(fd, S[5]); PUTS(fd, S[6]); PUTS(fd, S[7]); PUTS(fd, S[8]); PUTS(fd, S[9]); PRINT(fd, S[10]); PRINT(fd, #S); PUTS(fd, S[11]); close(fd); chmod(S[0], 0644); }
+MAIN(((char *[]){ "Grace_kid.c", "/*", "	Grace", "*/", "#include <fcntl.h> // open", "#include <unistd.h> // write", "#include <sys/stat.h> // chmod", "#define PRINT(FD, STR) for (int i = 0; STR[i]; i++) write(FD, &(STR[i]), 1);", "#define PUTS(FD, STR) PRINT(FD, STR) write(FD, (char[1]){10}, 1);", "#define MAIN(S) int main() { int fd = open(S[0], 514); PUTS(fd, S[1]); PUTS(fd, S[2]); PUTS(fd, S[3]); PUTS(fd, S[4]); PUTS(fd, S[5]); PUTS(fd, S[6]); PUTS(fd, S[7]); PUTS(fd, S[8]); PUTS(fd, S[9]); PRINT(fd, S[10]); PRINT(fd, #S); PUTS(fd, S[11]); close(fd); chmod(S[0], 0644); }", "MAIN(", ")" }))
